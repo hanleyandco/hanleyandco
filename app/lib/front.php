f79fff206dc55e319bf057f53272d54c27dab31b
Kristian Epps <kristian.epps@bbc.co.uk>
2014-08-25 21:07:16 +0100

    PHPunit tests now working

--- app/lib/front.php
+++ app/lib/front.php
@@ -5,6 +5,12 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+\Symfony\Component\Debug\ErrorHandler::register();
+\Symfony\Component\Debug\ExceptionHandler::register();
+
 // ...definitions ?
+$app['debug'] = true;
+
+require 'routes.php';
 
 $app->run();
