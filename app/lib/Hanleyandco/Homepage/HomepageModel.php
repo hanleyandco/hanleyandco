e7e238d2af13d6b99d74d4206b261803e2fdba22
Kristian Epps <kristian.epps@bbc.co.uk>
2014-12-12 08:49:00 +0000

    Added sections to view

--- app/lib/Hanleyandco/Homepage/HomepageModel.php
+++ app/lib/Hanleyandco/Homepage/HomepageModel.php
@@ -2,13 +2,19 @@
 
 namespace Hanleyandco\Homepage;
 
+use Hanleyandco\Util;
+
 class HomepageModel {
 
     private $_title;
+    private $_sections;
 
-    public function __construct()
+    public function __construct($title, $sections)
     {
-
+        $this->_title = $title;
+        foreach($sections as $section) {
+            $this->_sections[] = new SectionModel($section->header, $section->content);
+        }
     }
 
     public function setTitle($title) {
@@ -19,8 +25,49 @@ class HomepageModel {
         return $this->_title;
     }
 
-    public function getNavBar() {
-
+    /**
+     * @return mixed
+     */
+    public function getSections()
+    {
+        return $this->_sections;
     }
 
-} 
+}
+
+class SectionModel {
+
+    private $_title;
+    private $_id;
+    private $_content;
+
+    public function __construct($title, $content) {
+        $this->_title = $title;
+        $this->_id = Util::convertStringToId($title);
+        $this->_content = $content;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->_id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->_title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->_content;
+    }
+}
--- app/lib/Hanleyandco/XmlModelBuilder.php
+++ app/lib/Hanleyandco/XmlModelBuilder.php
@@ -4,14 +4,14 @@ namespace Hanleyandco;
 
 use Hanleyandco\Homepage\HomepageModel;
 use Hanleyandco\NavBar\NavBarModel;
+use Hanleyandco\Util;
 
 class XmlModelBuilder implements ModelBuilder {
 
     public function buildHomepageModel()
     {
         $data = simplexml_load_file(__DIR__.'/../../../static/content/home.xml');
-        $model = new HomepageModel();
-        $model->setTitle($data->title);
+        $model = new HomepageModel($data->title, $data->sections->section);
         return $model;
     }
 
@@ -20,14 +20,10 @@ class XmlModelBuilder implements ModelBuilder {
         $data = simplexml_load_file(__DIR__.'/../../../static/content/home.xml');
         $navBar = array();
         foreach($data->sections->section as $section) {
-            $navBar[] = array("name" => $this->convertToName($section[0]->header),
+            $navBar[] = array("name" => Util::convertStringToId($section[0]->header),
                               "title" => $section[0]->header);
         }
         $model = new NavBarModel($navBar);
         return $model;
-    }
-
-    function convertToName($string) {
-        return strtolower(preg_replace('/[\W]+/', '_', $string));
     }
 }
